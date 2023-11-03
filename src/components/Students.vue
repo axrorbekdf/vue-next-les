5bd7467f27c55e178705f9e119d4a8406ef23cff
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-03 11:38:44 +0500

    vue-nuxtjs

---
+++ src/components/Students.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div @click="bosildi">
+        {{name}}
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            required: true,
+            default: "noma'lum"
+        }
+    },
+    methods:{
+        bosildi(){
+            alert(`Name: ${this.name}`)
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/StudentView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Students :name="'Ahrorbek Davronov 1'"/>
+        <Students :name="'Ahrorbek Davronov 2'"/>
+        <Students :name="'Ahrorbek Davronov 3'"/>
+        <Students :name="'Ahrorbek Davronov 4'"/>
+        <Students :name="'Ahrorbek Davronov 5'"/>
+        <Students :name="'Ahrorbek Davronov 6'"/>
+        <Students/>
+    </div>
+</template>
+<script>
+import Students from '../components/Students.vue';
+export default {
+    components: {Students}
+}
+</script>
+<style>
+    
+</style>
