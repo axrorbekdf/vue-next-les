48e007b9731b0206bff10eaa0bee95cf978ea88d
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-09 10:24:09 +0500

    components

--- src/components/layout/CTA.vue
+++ src/components/layout/CTA.vue
@@ -1,5 +1,35 @@
 <template>
-    
+    <!-- CTA -->
+    <div class="relative z-10 -mt-48 pb-80 dark:bg-jacarta-900">
+                <div class="container">
+                <div class="relative overflow-hidden rounded-2.5xl px-16 pt-16 pb-8 shadow-md lg:px-24">
+                    <picture class="pointer-events-none absolute inset-0 -z-10 dark:hidden">
+                    <img src="img/gradient_light.jpg" alt="gradient" class="h-full w-full" />
+                    </picture>
+                    <picture class="pointer-events-none absolute inset-0 -z-10 hidden dark:block">
+                    <img src="img/gradient_dark.jpg" alt="gradient dark" class="h-full w-full" />
+                    </picture>
+                    <div class="items-center justify-between md:flex">
+                    <div class="mb-6 md:w-1/2">
+                        <h2 class="mb-4 font-display text-2xl text-jacarta-700 dark:text-white sm:text-3xl">
+                        Beginner
+                        </h2>
+                        <p class="mb-8 text-lg dark:text-jacarta-300">
+                        Our support heroes will have you up and running in less than 10mins.
+                        </p>
+                        <a
+                        href="contact.html"
+                        class="inline-block rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark"
+                        >
+                        Start learning
+                        </a>
+                    </div>
+                    <img src="img/crypto-consultant/cta_illustration.png" alt="" />
+                    </div>
+                </div>
+                </div>
+            </div>
+            <!-- end cta -->
 </template>
 <script>
 export default {
