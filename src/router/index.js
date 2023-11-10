82ef656333db25a45549efe224b92a6ba0645ee4
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-10 11:49:54 +0500

    middleware

--- src/router/index.js
+++ src/router/index.js
@@ -47,4 +47,22 @@ const router = createRouter({
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  if (!to.meta.middleware) {
+      return next()
+  }
+  const middleware = to.meta.middleware
+  const context = {
+      to,
+      from,
+      next,
+      store
+  }
+  return middleware[0]({
+      ...context,
+      next: middlewarePipeline(context, middleware, 1)
+  })
+})
+
+
 export default router
