3a70607e88d4052dc12f5f1fc25ea1ffae6525ee
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-11 15:15:02 +0500

    multi-language

--- src/main.js
+++ src/main.js
@@ -3,13 +3,26 @@ import App from './App.vue'
 import router from './router'
 import PageLoader from './mixins/PageLoader'
 import store from './store'
+import { createI18n } from "vue-i18n";
+// import translations
+import de from "./locales/de.json";
+import en from "./locales/en.json";
+
+// configure i18n
+const i18n = createI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages: { de, en },
+});
 
 import '@/assets/css/style.css'
 
 const app = createApp(App)
 // createApp.mixin(PageLoader)
 
+
 app.use(router)
 app.use(store)
+app.use(i18n);
 
 app.mount('#app')
