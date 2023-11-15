a781929d1c2acf2b7c54dab3d6deac07283a1a28
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-15 14:21:42 +0500

    skeleton loading

---
+++ src/components/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            userData: null
+        }
+    },
+    mounted(){
+        this.userData = this.loadUserData();
+    },
+    methods:{
+        loadUserData(){
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve({
+                        name: 'Matt Maribojoc',
+                        pic: 'https://cdn-images-1.medium.com/fit/c/100/100/2*EcZb9cndrhTF7_d74dv2Fg.png',
+                        bio: 'I run a VueJS community over at https://learnvue.co, develop web sites, and post whatever I find cool on the Internet.',
+                    })
+                }, 4000)
+            })
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
